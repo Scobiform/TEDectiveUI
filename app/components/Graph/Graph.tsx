936c968eb67715df3e6b23e9de933151ab7260fb
siemer@fsfe.org <siemer qfsfe.org>
2023-08-17 18:43:37 +0200

    #11

--- app/components/Graph/Graph.tsx
+++ app/components/Graph/Graph.tsx
@@ -22,28 +22,37 @@ export interface GraphProps {
   setVisuals: any
   previewNode: NodeObject | null | undefined;
   setPreviewNode: any;
+  isOpen: boolean;
+  setOpen: any;
 }
 
 // Create a component that will render the graph
-const Graph = ({graphData, physics, setPhysics, visuals, setVisuals, previewNode, setPreviewNode }: GraphProps) => {
+const Graph = ({graphData, physics, setPhysics, visuals, setVisuals,
+  previewNode, setPreviewNode, isOpen, setOpen }: GraphProps) => {
   
   const [width, height] = useWindowSize();
   
-  // Create a state variable to store the filter parameters
+  // State variable to store the filter parameters
   [physics, setPhysics] = useState(initialPhysics);
-  // Create a state variable to store the visual parameters
+  // State variable to store the visual parameters
   [visuals, setVisuals] = useState(initialVisuals);
-  // Create a state variable to store the node that is currently being previewed
+  // State variable to store the node that is currently being previewed
   [previewNode, setPreviewNode] = useState<NodeObject | null | undefined>(null);
+  // State variable to store whether the node panel is open or not
+  [isOpen, setOpen] = useState(false);
 
   // Create a reference to the forceGraphMethods
   const fgRef = useRef();
 
   // Callback function that will be called when a node is clicked
   const handleClick = useCallback(
-    (node: NodeObject) => setPreviewNode(node),
-    [fgRef]
+    (node: NodeObject) => {
+      setPreviewNode(node);
+      setOpen(true);
+    },
+    [isOpen]
   );
+  //console.log(isOpen);
 
   // D3 simulation settings
   // https://github.com/d3/d3-force
@@ -89,7 +98,7 @@ const Graph = ({graphData, physics, setPhysics, visuals, setVisuals, previewNode
           height={height}
         />
       </div>
-      <NodePanel previewNode={previewNode}/>
+      <NodePanel previewNode={previewNode} isOpen={isOpen} setOpen={setOpen}/>
       <GUI physics={physics} setPhysics={setPhysics} visuals={visuals} setVisuals={setVisuals}/>
     </>
   );
