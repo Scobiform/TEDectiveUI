27d24d2643faa5c125aa92ed78eb3f888cd712a6
Uwe Siemer <uwe@scobiform.com>
2023-08-22 20:23:17 +0200

    #12

--- components/Static/Header.tsx
+++ components/Static/Header.tsx
@@ -4,11 +4,12 @@ const Header = () =>
 {
     // Get the site name from the environment variable
     const appName = process.env.NEXT_PUBLIC_SITE_NAME;
+    const subString = appName?.substring(0, 3);
     
     return (
       <>
         <header>
-          <p>{appName}</p>
+          <p><b>{subString}</b>{appName?.substring(3, appName.length)}</p>
         </header>
       </>
     );
