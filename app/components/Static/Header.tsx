af5c8db71f6040b3ea5f5bc4156b8c0a1bbe8c2b
siemer@fsfe.org <siemer qfsfe.org>
2023-08-22 12:34:07 +0200

    #16

--- app/components/Static/Header.tsx
+++ app/components/Static/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Logo from './Logo';
+import { stringify } from 'querystring';
 
 const Header = () =>
 {
     // Get the site name from the environment variable
     const appName = process.env.NEXT_PUBLIC_SITE_NAME;
-
+    
     return (
       <>
         <header>
