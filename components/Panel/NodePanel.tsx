5cac1bce65d9a5e6a3b94cc72c334b14576cbbed
siemer@fsfe.org <siemer qfsfe.org>
2023-09-01 12:21:13 +0200

    #26

--- components/Panel/NodePanel.tsx
+++ components/Panel/NodePanel.tsx
@@ -74,7 +74,7 @@ const NodePanel = ({previewNode, isOpen, setOpen}: NodePanelProps) => {
                   {/* Switch case for node type */}  
                   {renderContentBasedOnNodeType(previewNode)}
 
-                  {objectString}
+                  {/*objectString*/}
 
                   {/* <DoughnutChart data={doughnutData} /> */}
                 </div>
@@ -89,25 +89,95 @@ const NodePanel = ({previewNode, isOpen, setOpen}: NodePanelProps) => {
 
 export default NodePanel;
 
+// Function to render content based on node type
 function renderContentBasedOnNodeType(previewNode: any) {
-  if(previewNode.tag !== undefined && previewNode.tag[1] === 'contract') {
-    return <div> FETCH CONTRACT
-              <h2>{previewNode.titla}</h2>
-              <p>{previewNode.description}</p>
+  // TODO: Build switch case for template display based on node type
+  // Make the detail view for each node type a component
+
+  // Fetch award
+  if(previewNode.awardID !== undefined){
+    return <div> {/* FETCH AWARD */}
+              <h2>💰 {previewNode.label}</h2>
+              <p>{previewNode.value.amount} {previewNode.value.currency}</p>
+              <button>{previewNode.id}</button>
            </div>;
   }
 
-  switch (previewNode.nodeType) {
-    case 'type1':
-      return <div>Content for type 1</div>;
-    case 'type2':
-      return <div>Content for type 2</div>;
-    case 'type3':
-      return <div>Content for type 3</div>;
-    default:
-      return <div>
-        <h2>{previewNode.titla}</h2>
-        <p>{previewNode.description}</p>
-      </div>;
+  // Fetch contract
+  if(previewNode.tag !== undefined && previewNode.tag[1] === 'contract') {
+    return <div> {/* FETCH CONTRACT */}
+              <button>{previewNode.id}</button>
+           </div>;
+  }
+
+  // Fetch tender
+  if(previewNode.tag !== undefined ) {
+    switch (previewNode.tag[0]) {
+      case 'tender':
+          return <div> 
+                    {/* FETCH TENDER*/}
+                    <button>{previewNode.id}</button>
+                </div>;
+          break;
+      case 'planning':
+          return <div> 
+                    {/* FETCH PLANNING*/}
+                    <button>{previewNode.id}</button>
+                </div>;
+          break;
+    }
+  }
+
+  // Fetch organization
+  // TODO: Make organizationDetails component
+  if(previewNode.name !== undefined) {
+    return <div> {/* FETCH ORGANIZATION*/}
+              <h2>🏢 {previewNode.name}</h2>
+              <p>ID: {previewNode.id}</p>
+              <p>Legal Name: {previewNode.identifier.legalName}</p>
+              <p>Country Code: {previewNode.address.countryCode}</p>
+              <p>Locality: {previewNode.address.locality}</p>
+              <p>Postal Code: {previewNode.address.postalCode}</p>
+              <p>Region: {previewNode.address.region}</p>
+              <p>Street Address: {previewNode.address.streetAddress}</p>
+           </div>;
+  }
+
+  // Return content based on node status
+  if(previewNode.status !== undefined)
+  {
+    switch (previewNode.status) {
+      case 'active':
+        return <div>
+                  <h2>🟢 {previewNode.label}</h2>
+                  <button>{previewNode.id}</button>
+                </div>;
+      case 'cancelled':
+        return <div>Content for type 2</div>;
+      case 'unsuccessful':
+        return <div>
+                  <h2>❌ {previewNode.label}</h2>
+                  <button>{previewNode.id}</button>
+               </div>;
+      case 'complete':
+        return <div>
+                  <h2>✅ {previewNode.label}</h2>
+                  <p>{previewNode.description}</p>
+                  <button>{previewNode.id}</button>
+               </div>;
+      case 'withdrawn':
+        return <div>Content for type 5</div>;
+      case 'planned':
+        return <div>
+                  <h2>📝 {previewNode.label}</h2>
+                  <p>{previewNode.description}</p>
+                  <button>{previewNode.id}</button>
+               </div>;      
+      default:
+        return <div>
+          <h2>{previewNode.titla}</h2>
+          <p>{previewNode.description}</p>
+        </div>;
+    }
   }
 }
