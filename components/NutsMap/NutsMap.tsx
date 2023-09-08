2853c83834be87c2b72158f943fed8719987c3ad
Uwe Siemer <uwe@scobiform.com>
2023-09-08 08:56:42 +0200

    #35

--- components/NutsMap/NutsMap.tsx
+++ components/NutsMap/NutsMap.tsx
@@ -15,31 +15,61 @@ const NutsMap = ({ data }: any) => {
     top: '2.8rem',
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const markers = await Promise.all(
+        data.map(async (item: any, index: number) => {
+          if (item.name && item.address && item.address.locality !== "") {
+            try {
+              // Fetch geocoding data for the locality
+              const response = await fetch('api/geocode?q=' + item.address.locality);
+
+              if (response.ok) {
+                const geoData = await response.json();
+                console.log(geoData);
+                return (
+                  <Marker key={item.id} position={[geoData[0].lat, geoData[0].lon]}>
+                    <Popup>
+                      <>
+                        <ul>
+                          <li>{item.name}</li>
+                          <li>{item.address.locality}</li>
+                          <li>{item.address.postalCode}</li>
+                          <li>{item.address.streetAddress}</li>
+                        </ul>
+                      </>
+                    </Popup>
+                  </Marker>
+                );
+              }
+            } catch (error) {
+              console.error('Error fetching geocoding data:', error);
+            }
+          }
+          return null;
+        })
+      );
+
+      // Filter out null markers (failed geocoding)
+      const filteredMarkers = markers.filter((marker) => marker !== null);
+
+      // Update the state with the markers
+      setMarkers(filteredMarkers);
+    };
+
+    // Fetch data initially
+    fetchData();
+  }, [data]);
+
+  const [markers, setMarkers] = useState<JSX.Element[]>([]);
+
   return (
     <div className={styles.map}>
       <MapContainer center={europeCenter} zoom={4} style={mapStyle} minZoom={2} attributionControl={false} zoomControl={false}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-         {data.map((item: any, index: number) => {
-          if (item.name && item.address) {
-            return (
-              <Marker key={item.id} position={[51.505 + item.index / 100, 10.09 + item.index / 100]}>
-                <Popup>
-                  <>
-                    <ul>
-                      <li>{item.name}</li>
-                      <li>{item.address.locality}</li>
-                      <li>{item.address.postalCode}</li>
-                      <li>{item.address.streetAddress}</li>
-                    </ul>
-                  </>
-                </Popup>
-              </Marker>
-            );
-          }
-          return null; // Don't render a marker for items that don't meet the criteria
-        })}
+        {markers}
       </MapContainer>
     </div>
   );
