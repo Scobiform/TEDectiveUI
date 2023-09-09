99ff0bd7400ffe276dee026f240cc7bb4d22fbff
Uwe Siemer <uwe@scobiform.com>
2023-09-09 16:07:53 +0200

    #12 #37

--- components/Static/ThemeSwitch.tsx
+++ components/Static/ThemeSwitch.tsx
@@ -1,17 +1,36 @@
-import { useEffect, useState } from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 
 const ThemeSwitch = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [buttonKey, setButtonKey] = useState(0);
 
   const toggleTheme = () => {
     const newTheme = !isDarkMode;
     setIsDarkMode(newTheme);
     document.documentElement.setAttribute('data-theme', newTheme ? 'dark' : 'light');
+    // Store the theme preference in localStorage (client-side only)
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', newTheme ? 'dark' : 'light');
+    }
+
+    // Increment the button key to force a re-render
+    setButtonKey(buttonKey + 1);
   };
 
+  useEffect(() => {
+    // Get the theme preference from localStorage (client-side only)
+    if (typeof window !== 'undefined') {
+      const storedTheme = localStorage.getItem('theme');
+      if (storedTheme) {
+        setIsDarkMode(storedTheme === 'dark');
+      }
+    }
+  }, []);
+
   return (
-    <button onClick={toggleTheme}>
-        {isDarkMode ? '🌞' : '🌚'}
+    <button key={buttonKey} onClick={toggleTheme}>
+      {isDarkMode ? '🌞' : '🌚'}
     </button>
   );
 };
