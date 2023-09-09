a5e245b7230d933d8b8696dd0dd89d8541bdc24f
Uwe Siemer <uwe@scobiform.com>
2023-09-09 17:13:11 +0200

    #13 #37

--- components/Static/ThemeSwitch.tsx
+++ components/Static/ThemeSwitch.tsx
@@ -1,40 +1,32 @@
 import React, { useEffect, useState } from 'react';
 
 const ThemeSwitch = () => {
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    if (typeof window !== 'undefined') {
-      const preferredColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
-      return preferredColorScheme.matches;
-    }
-    return false; // Default to dark mode if the window object is not available
-  });
-  const [buttonKey, setButtonKey] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleTheme = () => {
     const newTheme = !isDarkMode;
     setIsDarkMode(newTheme);
     document.documentElement.setAttribute('data-theme', newTheme ? 'dark' : 'light');
     // Store the theme preference in localStorage (client-side only)
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('theme', newTheme ? 'dark' : 'light');
-    }
-
-    // Increment the button key to force a re-render
-    setButtonKey(buttonKey + 1);
+    localStorage.setItem('theme', newTheme ? 'dark' : 'light');
   };
 
   useEffect(() => {
     // Get the theme preference from localStorage (client-side only)
-    if (typeof window !== 'undefined') {
-      const storedTheme = localStorage.getItem('theme');
-      if (storedTheme) {
-        setIsDarkMode(storedTheme === 'dark');
-      }
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setIsDarkMode(storedTheme === 'dark');
+      document.documentElement.setAttribute('data-theme', storedTheme);
+    } else {
+      // If no theme is stored, use prefers-color-scheme
+      const preferredColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+      setIsDarkMode(preferredColorScheme.matches);
+      document.documentElement.setAttribute('data-theme', preferredColorScheme.matches ? 'dark' : 'light');
     }
   }, []);
 
   return (
-    <button key={buttonKey} onClick={toggleTheme}>
+    <button onClick={toggleTheme}>
       {isDarkMode ? '🌞' : '🌚'}
     </button>
   );
