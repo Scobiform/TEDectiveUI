ceaf989912e3895f7f2c1d47854e4cfd185827e6
siemer@fsfe.org <siemer qfsfe.org>
2023-09-14 16:43:53 +0200

    #13

--- pages/index.tsx
+++ pages/index.tsx
@@ -260,15 +260,7 @@ const Home = ({apiPath, setApiPath, physics, setPhysics, visuals, setVisuals, pr
           label: 'Status Data',
           data: Object.values(statusCounts),
           backgroundColor: backgroundColors,
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(255, 205, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(54, 162, 235, 1)',
-          ],
-          borderWidth: 1,
+          borderWidth: 0,
         },
       ],
     };
