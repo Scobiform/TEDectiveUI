f53924c33de6a3602a8db88becc2ea3614036d8f
Uwe Siemer <uwe@scobiform.com>
2023-09-27 18:30:44 +0200

    #50

--- components/Cache/CachedObjects.tsx
+++ components/Cache/CachedObjects.tsx
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import styles from './cache.module.css';
 
+interface CachedObject {
+  name: ReactNode;
+  type: string; // Add 'type' field to CachedObject
+  place_id: undefined;
+  buyerData: undefined;
+  data: any;
+}
+
 function CachedObjects() {
-  const [cachedData, setCachedData] = useState([]);
+  const [cachedData, setCachedData] = useState<CachedObject[]>([]);
+  const [searchCache, setSearchCache] = useState<CachedObject[]>([]);
+  const [graphCache, setGraphCache] = useState<CachedObject[]>([]);
+  const [geocodeCache, setGeocodeCache] = useState<CachedObject[]>([]);
 
   useEffect(() => {
     async function fetchCachedData() {
@@ -11,7 +22,34 @@ function CachedObjects() {
         if (!response.ok) {
           throw new Error(`Failed to fetch data: ${response.status}`);
         }
-        const data = await response.json();
+        const data: CachedObject[] = await response.json();
+
+        // Sort cached data into separate lists based on their type
+        const searchList: CachedObject[] = [];
+        const graphList: CachedObject[] = [];
+        const geocodeList: CachedObject[] = [];
+
+        data.forEach((item: CachedObject) => {
+          switch (item.type) {
+            case 'search':
+              searchList.push(item);
+              break;
+            case 'graph':
+              graphList.push(item);
+              break;
+            case 'geocode':
+              geocodeList.push(item);
+              break;
+            default:
+              // Handle unexpected types if necessary
+              console.warn(`Unexpected type in cached object: ${item.type}`);
+              break;
+          }
+        });
+
+        setSearchCache(searchList);
+        setGraphCache(graphList);
+        setGeocodeCache(geocodeList);
         setCachedData(data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -25,8 +63,26 @@ function CachedObjects() {
   return (
     <div className={styles.cachedObjects}>
       <h2>Cached Objects</h2>
+      <h3>Search Cache</h3>
       <ul>
-        {cachedData.map((data, index) => (
+        {searchCache.map((data, index) => (
+          <li key={index}>
+            <p>{JSON.stringify(data, null, 2)}</p>
+          </li>
+        ))}
+      </ul>
+      <h3>Graph Cache</h3>
+      <ul>
+        {graphCache.map((data, index) => (
+          <li key={index}>
+            <p>{JSON.stringify(data, null, 2)}</p>
+          </li>
+        ))}
+      </ul>
+
+      <h3>Geocode Cache</h3>
+      <ul>
+        {geocodeCache.map((data, index) => (
           <li key={index}>
             <p>{JSON.stringify(data, null, 2)}</p>
           </li>
--- pages/api/cachedObjects.tsx
+++ pages/api/cachedObjects.tsx
@@ -2,27 +2,37 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs/promises';
 import path from 'path';
 
-const cacheDirectories = [
+const cacheDirectories: string[] = [
   './public/cache/geocode',
   './public/cache/search',
   './public/cache/graph',
 ];
 
+interface CachedData {
+  type: string;
+  [key: string]: any;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const cachedFileNames: string[] = [];
+  const cachedData: CachedData[] = [];
 
   for (const cacheDirectory of cacheDirectories) {
     try {
       const files = await fs.readdir(cacheDirectory);
 
       for (const file of files) {
-        // Add the filename to the list
-        cachedFileNames.push(file);
+        const filePath = path.join(cacheDirectory, file);
+        const cachedContent = await fs.readFile(filePath, 'utf8');
+        const parsedData = JSON.parse(cachedContent);
+
+        // Add the 'type' field to each cached object based on the cacheDirectory
+        const type = cacheDirectory.split('/').pop(); // Get the last segment as 'type'
+        cachedData.push({ type, ...parsedData });
       }
     } catch (error) {
       console.error('Error reading cached files:', error);
     }
   }
 
-  res.status(200).json(cachedFileNames);
+  res.status(200).json(cachedData);
 }
