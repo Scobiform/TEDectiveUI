48fcbf29486acf24e2442c129d4306f2f39be88a
Uwe Siemer <uwe@scobiform.com>
2023-09-27 15:44:54 +0200

    #31

--- pages/api/graph.tsx
+++ pages/api/graph.tsx
@@ -1,25 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs/promises'; // Import the promises-based file system module
+import path from 'path'; // Import the path module
 
-// Create an object to cache responses
-const cache: { [key: string]: any } = {};
+// Define the directory for cached data
+const cacheDirectory = path.join(process.cwd(), './public/cache');
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse) {
-  // Get the query parameter from the request
+// Function to ensure the cache directory exists
+async function ensureCacheDirectoryExists() {
+  try {
+    await fs.mkdir(cacheDirectory, { recursive: true });
+  } catch (error) {
+    console.error('Error creating cache directory:', error);
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { q } = req.query as { q: string };
 
   if (!q) {
     return res.status(400).json({ error: 'Missing query parameter' });
   }
 
+  // Ensure that the cache directory exists
+  await ensureCacheDirectoryExists();
+
   // Check if the response is already cached
-  if (cache[q]) {
-    return res.status(200).json(cache[q]);
+  const cacheFilePath = path.join(cacheDirectory, `${q}.json`);
+  try {
+    const cachedData = await fs.readFile(cacheFilePath, 'utf8');
+    const parsedCachedData = JSON.parse(cachedData);
+    return res.status(200).json(parsedCachedData);
+  } catch (cacheReadError) {
+    // If cached data is not available or there's an error reading it, fetch and cache new data.
   }
 
-  // API URL
   const apiURL = process.env.NEXT_PUBLIC_API_URL;
-
-  // Graph paths
   const buyerGraphPath = apiURL + 'graph/releases/buyer/';
   const supplierGraphPath = apiURL + 'graph/releases/supplier/';
 
@@ -38,13 +53,12 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
       supplierResponse.json(),
     ]);
 
-    // Cache the response
-    cache[q] = { buyerData, supplierData };
+    // Cache the response by writing it to a file
+    await fs.writeFile(cacheFilePath, JSON.stringify({ buyerData, supplierData }), 'utf8');
 
-    // Respond with the fetched data
     res.status(200).json({ buyerData, supplierData });
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
-};
+}
