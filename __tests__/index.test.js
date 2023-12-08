89e37f76007b3256e15deffb0c05da4000182170
Uwe Siemer <uwe@scobiform.com>
2023-12-08 09:51:19 +0100

    #1 #4

---
+++ __tests__/index.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+import GraphWrapper from '../components/Graph/GraphWrapper';
+import '@testing-library/jest-dom'
+ 
+describe('Home', () => {
+  it('CHeck if pathname is changed to id route', () => {
+    render(<Home />)
+ 
+    // Check route with jest
+    //expect(window.location.pathname).toBe('/1778f0d1-545c-5fcb-bf80-7c0512bbd0be')
+
+    // Change route with jest
+    window.history.pushState({}, '', '/c85f969c-f516-5e13-b1bd-3df7abe48531');
+
+    // Check route with jest
+    expect(window.location.pathname).toBe('/c85f969c-f516-5e13-b1bd-3df7abe48531')
+
+  })
+})
